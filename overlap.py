e3ea4088accd44d2dc553af8c20f426d229b26bf
Andrew Quitadamo <aquitada@uncc.edu>
2014-11-07 13:25:00 -0500

    Moved position index loop, large speed up

--- overlap.py
+++ overlap.py
@@ -7,35 +7,33 @@ pos_dict = {}
 
 
 for x in range (1,len(sys.argv)):
-    #print sys.argv[x]
     with open(sys.argv[x], 'r') as f:
         header = f.readline().rstrip()
-        #print(first_line)
         ids=header.split("\t")
         i=0
-        for vals in ids:
+        for id in ids:
             i+=1
-            if vals in id_dict:
-                id_dict[vals]+=1
+            if id in id_dict:
+                id_dict[id]+=1
             else:
-                id_dict[vals]=1
+                id_dict[id]=1
 
-            if vals in pos_dict:
-                pos_dict[vals]+=","+str(i)
+            if id in pos_dict:
+                pos_dict[id]+=","+str(i)
             else:
-                pos_dict[vals]=str(i)
+                pos_dict[id]=str(i)
 
 for x in range (1,len(sys.argv)):
     with open(sys.argv[x], 'r') as f:
+        pos = []
+        for key in id_dict:
+            if id_dict[key]==len(sys.argv)-1:
+                ind = pos_dict[key].split(",")
+                pos.append(int(ind[x-1]))
         with open(str(sys.argv[x]+'.out'),'w') as fo:
             for line in f:
-                li = line.rstrip()
-                temp = li.split("\t")
-                pos = []
-                for key in id_dict:
-                    if id_dict[key]==len(sys.argv)-1:
-                        ind = pos_dict[key].split(",")
-                        pos.append(int(ind[x-1]))
+                line = line.rstrip()
+                temp = line.split("\t")
                 new_line=[temp[y-1] for y in pos] 
                 new_line_str="\t".join(new_line)
                 print(new_line_str, file=fo)
